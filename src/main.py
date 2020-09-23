d905caf4b1566cea20e887253087aa2e8936167b
Jeroen Vermeylen <jeroen@vermeylen.org>
2020-09-23 14:48:44 +0200

    add local hook for shelly

--- src/main.py
+++ src/main.py
@@ -38,3 +38,21 @@ async def shelly_get(location: str = ""):
         response = "Toggled bedroom lights."
     print(response)
     return response
+
+@app.get("/shellylocal")
+async def shelly_get(location: str = ""):
+    response = "Done nothing!"
+    if str.upper(location) == 'HALLDOWN':
+        url = 'http://192.168.1.4:8123/api/webhook/alloff'
+        requests.post(url)
+        response = "Turned everything off."
+    elif str.upper(location) == 'HALLUP':
+        url = 'http://192.168.1.4:8123/api/webhook/halltoggle'
+        requests.post(url)
+        response = "Toggled hall lights."
+    elif str.upper(location) == 'BEDROOM':
+        url = 'http://192.168.1.4:8123/api/webhook/bedroomtoggle'
+        requests.post(url)
+        response = "Toggled bedroom lights."
+    print(response)
+    return response
