55c24847ae67d60da50d9605cabcfe359969a367
Jeroen Vermeylen <jeroen@vermeylen.org>
2020-09-23 20:18:24 +0200

    move shelly needs to own branch

--- src/main.py
+++ src/main.py
@@ -7,38 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def index_get():
     return "Hello World!"
-
-@app.post("/hook")
-async def hook_post(request: Request):
-    body_json = await request.json()
-    if body_json['intent']['name'] == 'HomeSweetOff':
-        url = 'https://monnikenhof.duckdns.org:8001/api/webhook/alloff'
-        requests.post(url)
-        print("Turned everything off.")
-    elif body_json['intent']['name'] == 'HomeSweetCast':
-        url = 'https://monnikenhof.duckdns.org:8001/api/webhook/tvtime'
-        requests.post(url)
-        print("Turned on cast.")
-    return "Hello World!"
-
-@app.get("/shelly")
-async def shelly_get(location: str = ""):
-    response = "Done nothing!"
-    if str.upper(location) == 'HALLDOWN':
-        url = 'https://monnikenhof.duckdns.org:8001/api/webhook/alloff'
-        requests.post(url)
-        response = "Turned everything off."
-    elif str.upper(location) == 'HALLUP':
-        url = 'https://monnikenhof.duckdns.org:8001/api/webhook/halltoggle'
-        requests.post(url)
-        response = "Toggled hall lights."
-    elif str.upper(location) == 'BEDROOM':
-        url = 'https://monnikenhof.duckdns.org:8001/api/webhook/bedroomtoggle'
-        requests.post(url)
-        response = "Toggled bedroom lights."
-    print(response)
-    return response
-
+    
 @app.get("/shellylocal")
 async def shellylocal_get(location: str = ""):
     response = "Done nothing!"
